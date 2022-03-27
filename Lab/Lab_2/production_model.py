7d161aa2503c6582ba953e42612130d1f01010b7
cooperhankun <cooperhanson@gmail.com>
2022-03-27 17:15:47 +0200

    lab_2 done

--- Lab/Lab_2/production_model.py
+++ Lab/Lab_2/production_model.py
@@ -1,10 +1,12 @@
 import joblib
 import pandas as pd
 
+# Load the model and samples
 df = pd.read_csv("test_samples.csv")
 LR_deploy_model = joblib.load("LR_deploy_model")
 
-predictions = pd.DataFrame(LR_deploy_model.predict_proba(scaled_X_test), columns=["probability class 0", "probability class 1"])
-predictions["prediction"] = LR_deploy_model.predict(scaled_X_test)
+# apply the model to the samples
+predictions = pd.DataFrame(LR_deploy_model.predict_proba(df), columns=["probability class 0", "probability class 1"])
+predictions["prediction"] = LR_deploy_model.predict(df)
 
 predictions.to_csv("prediction.csv", index=False)
